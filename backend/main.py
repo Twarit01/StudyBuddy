0155cf0e0da9a4ea26042b8961eca145d7c71a28
Twarit Shukla <twaritshukla@Twarits-MacBook-Air.local>
2026-07-09 15:34:04 +0530

    URL fixing

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:3000",
-        "https://study-buddy-lac-eta.vercel.app",
+        "https://ai-studybuddyy.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
